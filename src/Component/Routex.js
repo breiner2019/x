e15a9fddc34167c22c3f4e0e58362013120a75ab
breiner2019 <elbreym13@gmail.com>
2024-09-19 11:17:39 -0500

    edit

--- src/Component/Routex.js
+++ src/Component/Routex.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import { Com1 } from './Com1';
 import { Watch } from './Watch';
 import { Search } from './Search';
-import BrowserRouter from 'react-router-dom/BrowserRouter'; 
 
-export const Routex  = () => {
+export const Routex = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-    
     <Routes>
-        <Route path="/" element={<Com1></Com1>}></Route>
-        <Route path="/search" element={<Search></Search>}></Route>
-        <Route path="/watch/:id" element={<Watch></Watch>}></Route>
+      <Route exact path='/' render={()=><Com1></Com1>} />
+      <Route path='/watch/:id' render={()=><Watch></Watch>}/>
+      <Route path='/search' render={()=><Search></Search>}/>
     </Routes>
-    
-    </BrowserRouter>
-  )
-}
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css'; // Your styles
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  
+  </BrowserRouter>,
+  document.getElementById('root')
 );
